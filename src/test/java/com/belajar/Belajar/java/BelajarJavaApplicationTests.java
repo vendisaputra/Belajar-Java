4d586e123c300547f8e257ff6c473a47740f427b
vendisaputra <vendisaputra@gmail.com>
2019-11-04 11:41:25 +0700

    Upload To Git

---
+++ src/test/java/com/belajar/Belajar/java/BelajarJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.Belajar.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
