1478f2a663a81fa6d544593d02ec01553bb00eb4
vendisaputra <vendisaputra@gmail.com>
2019-11-04 14:58:12 +0700

    fix CRUD on table mahasiswa

--- src/main/java/com/belajar/Belajar/java/controller/MahasiswaController.java
+++ src/main/java/com/belajar/Belajar/java/controller/MahasiswaController.java
@@ -2,12 +2,14 @@ package com.belajar.Belajar.java.controller;
 
 import com.belajar.Belajar.java.entity.Mahasiswa;
 import com.belajar.Belajar.java.service.MahasiswaService;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/mahasiswa")
@@ -28,6 +30,35 @@ public class MahasiswaController {
     @PostMapping
     public ResponseEntity create(@Valid @RequestBody Mahasiswa mahasiswa){
         return ResponseEntity.ok(mahasiswaService.save(mahasiswa));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Mahasiswa> findById(@PathVariable int id){
+        Optional<Mahasiswa> mahasiswa = mahasiswaService.findById(id);
+        if(!mahasiswa.isPresent()){
+            System.out.println("Id " + id + " gak enek!");
+            ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(mahasiswa.get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Mahasiswa> update(@PathVariable int id, @Valid @RequestBody Mahasiswa mahasiswa){
+        if(!mahasiswaService.findById(id).isPresent()){
+            ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(mahasiswaService.save(mahasiswa));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable int id){
+        if(!mahasiswaService.findById(id).isPresent()){
+            ResponseEntity.badRequest().build();
+        }else{
+            mahasiswaService.deleteById(id);
+        }
+        return ResponseEntity.ok().build();
+
     }
 
 
